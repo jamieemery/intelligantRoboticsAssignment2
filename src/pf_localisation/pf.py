995a69c125dc219439f0de3330ace213437ee706
Jamie Emery <d.jamie.emery@gmail.com>
2020-11-16 12:25:14 +0000

    Margarita's changes to code to test why the localisation isn't accurate

--- src/pf_localisation/pf.py
+++ src/pf_localisation/pf.py
@@ -32,17 +32,8 @@ sigma_x2 = 1 #variance for x
 sigma_xy = 0 #---correlation for xy
 sigma_yx = 0 #---and yx
 sigma_y2 = 1 #variance for y
-sigma = numpy.array([[sigma_x2,sigma_xy],[sigma_yx,sigma_y2]]) #covariance matrix
-sigma_inv = numpy.linalg.inv(sigma) #inverse matrix of the covariance matrix
-
-#global x_signal # our signal values
-#global u_control # control signal
-#global noise # processed noise
-#global z # measurement values
-#global v # measurement noise
-
-#x_signal = A*x_signal + B*u_control + noise
-#z = H*x_signal + v
+actualSigma = numpy.array([[sigma_x2,sigma_xy],[sigma_yx,sigma_y2]])
+sigma_inv = numpy.linalg.inv(actualSigma)
 
 # Our 2-dimensional distribution will be over variables X and Y
 N = 60
@@ -82,11 +73,11 @@ class PFLocaliser(PFLocaliserBase):
 
 
     # the update function
-    def change_mean_sigma(mean1, sigma1, mean2, sigma2):
+    def change_mean_sigma(self, mean1, sigma1, mean2, sigma2):
         new_mean = (sigma2*mean1 + sigma1*mean2)/(sigma2+sigma1)
         new_sigma = 1/(1/sigma2 + 1/sigma1)
 
-        return [new_mean, new_sigma]
+        return new_mean, new_sigma
 
 
     # the motion update/predict function
@@ -113,57 +104,19 @@ class PFLocaliser(PFLocaliserBase):
         :Return:
             | (geometry_msgs.msg.PoseArray) poses of the particles
         """
-        # ----- Initialize the particle cloud as an empty array
-        #self.particlecloud = PoseArray()
 
-        """Create the noise to multiply by the random Gaussian number that will
-        get added to each of the Poses, that are set to a random position
-        and orientation around the initial pose"""
-        #sensorSigma=3 #variance
-        #sensorMu=0 #mean
-        #noise=sensorSigma * numpy.random.randn() + sensorMu
+	    self.particlecloud = PoseArray()
 
-        """Create a range for the ammount of random Gaussian values to generate """
-        #randomGauss = 10*self.NUMBER_PREDICTED_READINGS
-
-        #gaussianRandomNumX = []
-        #gaussianRandomNumY = []
-        #randomYawArray = []
-
-        #for i in range (0,randomGauss):
-            #gaussianRandomNumX.append(random.gauss(0,1))
-            #gaussianRandomNumY.append(random.gauss(0,1))
-            #x=random.randint(1,180)
-            #randomYaw=(math.pi/x)
-            #randomYawArray.append(randomYaw)
-
-        #iterator = 0
-
-        """
-    Set the particles to a random position and orientation around the initial pose
-        """
-        #particleNumber = 10**2 # 10**3 # 10**4 # 10**5 experiment with different ammounts of particles
-        self.particlecloud.poses = []
-        #while iterator < particleNumber:
-            #particle = Pose()
-            #particle.position.x = initialpose.pose.pose.position.x + (gaussianRandomNumX[iterator] * noise)
-            #particle.position.y = initialpose.pose.pose.position.y + (gaussianRandomNumY[iterator] * noise)
-            #particle.position.z = initialpose.pose.pose.position.z
-            #particle.orientation = rotateQuaternion(initialpose.pose.pose.orientation, randomYawArray[iterator])
-
-            #self.particlecloud.poses.append(particle)
-            #iterator += 1
         particle = Pose()
 
         mu_x = initialpose.pose.pose.position.x
         mu_y = initialpose.pose.pose.position.y
-        particle.position.x = mu_x
+	    particle.position.x = mu_x
         particle.position.y = mu_y
         particle.position.z = initialpose.pose.pose.position.z
-        particle.orientation = initialpose.pose.pose.orientation
-
-        self.particlecloud.poses.append(particle)
-
+	    particle.orientation = initialpose.pose.pose.orientation
+	    self.particlecloud.poses.append(particle)
+	    print(self.particlecloud.poses)
         return self.particlecloud
 
     def create_C(self, predictedMut, predictedLaserScans):
@@ -171,9 +124,9 @@ class PFLocaliser(PFLocaliserBase):
         #Ct = predictedLaserScans * numpy.linalg.inv(predictedMut)
 
         iterator = 0
-        for j in predictedLaserScans[0][:]:
-            ctx = j / predictedMut[0][0]
-            cty = j / predictedMut[1][0]
+        for j in predictedLaserScans:
+            ctx = (j / predictedMut[0][0])/2
+            cty = (j / predictedMut[1][0])/2
             Ct[iterator][0] = ctx
             Ct[iterator][1] = cty
             iterator += 1
@@ -181,27 +134,31 @@ class PFLocaliser(PFLocaliserBase):
 
     def createPredictedScan(self, predictedMut):
         predictedLaserScans = numpy.zeros((len(self.sensor_model.reading_points),1))
-        iter = 0
+	    iter = 0
         for i, obs_bearing in self.sensor_model.reading_points:
             # ----- Predict the scan according to the map
             map_range = self.sensor_model.calc_map_range(predictedMut[0][0], predictedMut[1][0],
                                      getHeading(self.particlecloud.poses[0].orientation) + obs_bearing)
+	    # ----- Laser reports max range as zero, so set it to range_max
+            if (map_range <= 0.0):
+                map_range = self.sensor_model.scan_range_max
+
             predictedLaserScans[iter][0] = map_range
-            iter += 1
+   	        iter += 1
         return predictedLaserScans
 
     def createActualScan(self, scan, scanMax):
         actualLaserScans = numpy.zeros((len(self.sensor_model.reading_points),1))
-        iter = 0
+	    iter = 0
         for i in self.sensor_model.reading_points:
             if math.isnan(scan[i[0]]):
                 actualLaserScans[iter][0] = scanMax
-            else:
+   	        else:
                 actualLaserScans[iter][0] = scan[i[0]]
-                iter += 1
+   	    iter += 1
         return actualLaserScans
 
-    def multivariate_gaussian(mu, Sigma):
+    def multivariate_gaussian(self, mu, Sigma):
         """Return the multivariate Gaussian distribution on array pos.
 
         pos is an array constructed by packing the meshed arrays of variables
@@ -220,42 +177,29 @@ class PFLocaliser(PFLocaliserBase):
         return numpy.exp(-fac / 2) / N
 
     def update_particle_cloud(self, scan):
-        predictedMut = numpy.zeros((2,1))
+
+	    global actualSigma
+	    Rt = numpy.cov(numpy.random.rand(2,2))
+	    Qt = numpy.cov(numpy.random.rand(20,20))
+	    predictedMut = numpy.zeros((2,1))
         predictedMut[0][0] = self.particlecloud.poses[0].position.x
         predictedMut[1][0] = self.particlecloud.poses[0].position.y
         predictedScan = self.createPredictedScan(predictedMut)
-
-        Rt = numpy.array([[10,0],[0,10]])
-        Qt = numpy.random.rand(20,20)*10
-        predictedSigma = sigma + Rt
+        predictedSigma = actualSigma + Rt
 
         Ct = self.create_C(predictedMut,predictedScan)
+	#print(Ct)
         zt = self.createActualScan(scan.ranges, scan.range_max)
-        #a = numpy.linalg.inv((numpy.dot(numpy.dot(Ct,predictedSigma),numpy.transpose(Ct)))+Qt)
-        #print(a.shape)
-        Kt = numpy.dot(numpy.dot(predictedSigma, numpy.transpose(Ct)), numpy.linalg.inv((numpy.dot(numpy.dot(Ct,predictedSigma),numpy.transpose(Ct)))+Qt))
+        Kt = numpy.dot(numpy.dot(predictedSigma, numpy.transpose(Ct)), numpy.linalg.inv(numpy.dot(numpy.dot(Ct,predictedSigma),numpy.transpose(Ct))+Qt))
         I = numpy.identity(2)
-        actualMut = predictedMut + numpy.dot(Kt,(zt - predictedScan))
+	#print(Kt)
+        actualMut = predictedMut + numpy.dot(Kt,(zt - numpy.dot(Ct,predictedMut)))
         actualSigma = numpy.dot((I-numpy.dot(Kt,Ct)),predictedSigma)
 
-        ##Plot the gaussian distribution
-        # The distribution on the variables X, Y packed into pos.
-        Z = self.multivariate_gaussian(actualMut, actualSigma)
+        self.particlecloud.poses[0].position.x = actualMut[0][0]
+	    self.particlecloud.poses[0].position.y = actualMut[1][0]
 
-        # Create a surface plot and projected filled contour plot under it.
-        fig = plt.figure()
-        ax = fig.gca(projection='3d')
-        ax.plot_surface(X, Y, Z, rstride=3, cstride=3, linewidth=1, antialiased=True,
-                       cmap=cm.viridis)
-
-        cset = ax.contourf(X, Y, Z, zdir='z', offset=-0.15, cmap=cm.viridis)
-
-        # Adjust the limits, ticks and view angle
-        ax.set_zlim(-0.15,0.2)
-        ax.set_zticks(numpy.linspace(0,0.2,5))
-        ax.view_init(27, -21)
-
-        plt.show()
+        print(actualMut-predictedMut)
 
     def estimate_pose(self):
         """
